8ada4676f695fb62e48e95c6bec3687571464a3b
Ilya Korchenov <ilyakorchenov@mail.ru>
2016-06-06 22:46:08 +0300

    add modaal and masonry plugins, add big images

--- src/js/script.js
+++ src/js/script.js
@@ -101,202 +101,224 @@ function validator() {
 }
 
 //Google-maps
-var styleArr = [
-    {
-        "featureType": "all",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#3e606f"
-            },
-            {
-                "weight": 2
-            },
-            {
-                "gamma": 0.84
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "weight": 0.6
-            },
-            {
-                "color": "#607892"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#4f6985"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#526d8a"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#607892"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#aabdce"
-            },
-            {
-                "lightness": "0"
-            },
-            {
-                "weight": "0.20"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "hue": "#0063ff"
-            },
-            {
-                "invert_lightness": true
-            },
-            {
-                "weight": "5.13"
-            },
-            {
-                "gamma": "8.43"
-            },
-            {
-                "lightness": "34"
-            },
-            {
-                "saturation": "48"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#406d80"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#5b7189"
-            }
-        ]
-    }
-];
+var google = google || false;
 
-google.maps.event.addDomListener(window, "load", init);
-
-function init() {
-    var e = {
-            zoom: 7,
-            center: new google.maps.LatLng(41.547859,-72.8765097,17),
-            styles: styleArr
+if(google){
+    var styleArr = [
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
         },
-        t = document.getElementById("map"),
-        i = new google.maps.Map(t, e);
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#3e606f"
+                },
+                {
+                    "weight": 2
+                },
+                {
+                    "gamma": 0.84
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "weight": 0.6
+                },
+                {
+                    "color": "#607892"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#4f6985"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#526d8a"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#607892"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#aabdce"
+                },
+                {
+                    "lightness": "0"
+                },
+                {
+                    "weight": "0.20"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "hue": "#0063ff"
+                },
+                {
+                    "invert_lightness": true
+                },
+                {
+                    "weight": "5.13"
+                },
+                {
+                    "gamma": "8.43"
+                },
+                {
+                    "lightness": "34"
+                },
+                {
+                    "saturation": "48"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#406d80"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#5b7189"
+                }
+            ]
+        }
+    ];
 
-    new google.maps.Marker({
-        position: new google.maps.LatLng(42.3540236,-71.0730505,15.88),
-        map: i,
-        title: "Fisher College",
-        icon: "img/svg/markpoint.svg"
-    });
+    google.maps.event.addDomListener(window, "load", init);
 
-    new google.maps.Marker({
-        position: new google.maps.LatLng(42.3770068,-71.1188488,17),
-        map: i,
-        title: "Harvard University",
-        icon: "img/svg/markpoint.svg"
-    });
+    function init() {
+        var e = {
+                zoom: 7,
+                center: new google.maps.LatLng(41.547859,-72.8765097,17),
+                styles: styleArr,
+                scaleControl: false,
+                mapTypeControl: false,
+                scrollwheel: false,
+                panControl: false,
+                streetViewControl: false
+            },
+            t = document.getElementById("map"),
+            i = new google.maps.Map(t, e);
 
-    new google.maps.Marker({
-        position: new google.maps.LatLng(40.8075395,-73.9647614,17),
-        map: i,
-        title: "Columbia University",
-        icon: "img/svg/markpoint.svg"
-    });
+        new google.maps.Marker({
+            position: new google.maps.LatLng(42.3540236,-71.0730505,15.88),
+            map: i,
+            title: "Fisher College",
+            icon: "img/svg/markpoint.svg"
+        });
 
-    new google.maps.Marker({
-        position: new google.maps.LatLng(42.3600949,-71.0963487),
-        map: i,
-        title: "Massachusetts Institute of Technology",
-        icon: "img/svg/markpoint.svg"
-    });
+        new google.maps.Marker({
+            position: new google.maps.LatLng(42.3770068,-71.1188488,17),
+            map: i,
+            title: "Harvard University",
+            icon: "img/svg/markpoint.svg"
+        });
 
-    new google.maps.Marker({
-        position: new google.maps.LatLng(40.7295174,-73.9986496,17),
-        map: i,
-        title: "New York University",
-        icon: "img/svg/markpoint.svg"
-    });
+        new google.maps.Marker({
+            position: new google.maps.LatLng(40.8075395,-73.9647614,17),
+            map: i,
+            title: "Columbia University",
+            icon: "img/svg/markpoint.svg"
+        });
+
+        new google.maps.Marker({
+            position: new google.maps.LatLng(42.3600949,-71.0963487),
+            map: i,
+            title: "Massachusetts Institute of Technology",
+            icon: "img/svg/markpoint.svg"
+        });
+
+        new google.maps.Marker({
+            position: new google.maps.LatLng(40.7295174,-73.9986496,17),
+            map: i,
+            title: "New York University",
+            icon: "img/svg/markpoint.svg"
+        });
+    }
 }
+
+//Masonry
+$('.photos__list').masonry({
+    itemSelector: '.photos__list-item',
+    columnWidth: 225,
+    gutter: 10
+});
+
+//Modaal
+$('.photos__link').modaal({
+    type: 'image',
+    animation: 'none'
+});
